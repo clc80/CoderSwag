36de02b9033aa6ebb3c9a99071cb9d211a959182
Claudia Maciel <clc_80@yahoo.com>
2021-01-21 23:57:18 -0800

    Implemented Unit Tests to Check that the right data was passed

--- CoderSwag/Services/DataService.swift
+++ CoderSwag/Services/DataService.swift
@@ -18,26 +18,26 @@ class DataService {
     ]
     
     private let hats = [
-        Product(description: "Devslopes Logo Graphic Beanie", imageName: "$18", price: "hat01.png"),
-        Product(description: "Devslopes Logo Hat Black", imageName: "$22", price: "hat02.png"),
-        Product(description: "Devslopes Logo Hat White", imageName: "$22", price: "hat03.png"),
-        Product(description: "Devslopes Logo Snapback", imageName: "$20", price: "hat04.png")
+        Product(description: "Devslopes Logo Graphic Beanie", imageName: "hat01.png", price:"$18"),
+        Product(description: "Devslopes Logo Hat Black", imageName: "hat02.png", price:"$22"),
+        Product(description: "Devslopes Logo Hat White", imageName: "hat03.png", price:"$22"),
+        Product(description: "Devslopes Logo Snapback", imageName: "hat04.png", price:"$20")
     ]
     
     private let hoodies = [
-        Product(description: "Devslopes Logo Hoodie Grey", imageName: "$32", price: "hoodie01.png"),
-        Product(description: "Devslopes Logo Hoodie Red", imageName: "$32", price: "hoodie02.png"),
-        Product(description: "Devslopes Hoodie Grey", imageName: "$32", price: "hoodie03.png"),
-        Product(description: "Devslopes Hoodie Black", imageName: "$32", price: "hoodie04.png")
+        Product(description: "Devslopes Logo Hoodie Grey", imageName: "hoodie01.png", price:"$32"),
+        Product(description: "Devslopes Logo Hoodie Red", imageName: "hoodie02.png", price:"$32"),
+        Product(description: "Devslopes Hoodie Grey", imageName: "hoodie03.png", price:"$32"),
+        Product(description: "Devslopes Hoodie Black", imageName: "hoodie04.png", price:"$32")
         
     ]
     
     private let shirts = [
-        Product(description: "Devslopes Logo Shirt Black", imageName: "$18", price: "shirt01.png"),
-        Product(description: "Devslopes Badge Shirt Light Grey", imageName: "$19", price: "shirt02.png"),
-        Product(description: "Devslopes Logo Shirt Red", imageName: "$18", price: "shirt03.png"),
-        Product(description: "Hustle Delegate Grey", imageName: "$18", price: "shirt04.png"),
-        Product(description: "Kickflip Studios Black", imageName: "$18", price: "shirt05.png")
+        Product(description: "Devslopes Logo Shirt Black", imageName: "shirt01.png", price:"$18"),
+        Product(description: "Devslopes Badge Shirt Light Grey", imageName: "shirt02.png", price:"$19"),
+        Product(description: "Devslopes Logo Shirt Red", imageName: "shirt03.png", price:"$18"),
+        Product(description: "Hustle Delegate Grey", imageName: "shirt04.png", price:"$18"),
+        Product(description: "Kickflip Studios Black", imageName: "shirt05.png", price:"$18")
     ]
     
     private let digitalGoods = [Product]()
--- CoderSwagTests/CoderSwagTests.swift
+++ CoderSwagTests/CoderSwagTests.swift
@@ -29,5 +29,40 @@ class CoderSwagTests: XCTestCase {
             // Put the code you want to measure the time of here.
         }
     }
+    
+    func testGetCategories() throws {
+        // Make sure we aren't getting back nil
+        XCTAssertNotNil(DataService.instance.getCategories())
+    }
+    
+    func testGetHats() throws {
+        let hat = Product(description: "Devslopes Logo Graphic Beanie", imageName: "hat01.png", price: "$18" )
+        // Make sure we aren't getting back nil
+        XCTAssertNotNil(DataService.instance.getHats())
+        
+        //Make sure we actually get the right data back
+        XCTAssertEqual(DataService.instance.getHats()[0].description, hat.description )
+    }
+    
+    func testGetShirts() throws {
+        let shirt = Product(description: "Kickflip Studios Black", imageName: "shirt05.png" , price: "$18" )
+        // Make sure we aren't getting back nil
+        XCTAssertNotNil(DataService.instance.getShirts())
+        
+        // Make sure we get the right data back
+        XCTAssertEqual(DataService.instance.getShirts()[4].price, shirt.price)
+    }
+    func testGetHoodies() throws {
+        let hoodie = Product(description: "Devslopes Logo Hoodie Grey", imageName: "hoodie01.png" , price: "$32")
+        // Make sure we aren't getting back nil
+        XCTAssertNotNil(DataService.instance.getHoodies())
+        
+        //Make sure we actually get the right data back
+        XCTAssertEqual(DataService.instance.getHoodies()[0].imageName, hoodie.imageName)
+    }
+    func testGetDigital() throws {
+        // Make sure we aren't getting back nil
+        XCTAssertNotNil(DataService.instance.getDigital())
+    }
 
 }
