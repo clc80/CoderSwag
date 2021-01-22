472837147d9d1a5ccfa00e3cf3cd174ff0ce53da
Claudia Maciel <clc_80@yahoo.com>
2021-01-21 23:19:29 -0800

    Created CollectionView to display products

---
+++ CoderSwag/View/ProductCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  ProductCollectionViewCell.swift
+//  CoderSwag
+//
+//  Created by Claudia Maciel on 1/21/21.
+//
+
+import UIKit
+
+class ProductCollectionViewCell: UICollectionViewCell {
+    // MARK: - Properties
+    @IBOutlet var productImage: UIImageView!
+    @IBOutlet var productDescription: UILabel!
+    @IBOutlet var productPrice: UILabel!
+    
+    
+}
